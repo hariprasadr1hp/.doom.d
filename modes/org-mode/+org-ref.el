c0439fa31e0cbb8551863bcb67e029670ee55fbb
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2021-03-29 22:23:25 +0200

    org-citations

---
+++ modes/org-mode/+org-ref.el
@@ -0,0 +1,8 @@
+;;; modes/org-mode/+org-ref.el -*- lexical-binding: t; -*-
+
+;;(setq reftex-default-bibliography '("~/my/references.bib"))
+
+;; see org-ref for use of these variables
+;; (setq org-ref-bibliography-notes "~/my/bibliography/notes.org"
+;;       org-ref-default-bibliography '("~/my/bibliography/references.bib")
+;;       org-ref-pdf-directory "~/my/bibliography/bibtex-pdfs/")
