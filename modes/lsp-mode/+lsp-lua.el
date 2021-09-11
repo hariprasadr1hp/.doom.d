b439fc7919c362204a407e6c9b16e8e36681c6a7
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2021-09-11 22:38:05 +0200

    lsp-lua settings updated

--- modes/lsp-mode/+lsp-lua.el
+++ modes/lsp-mode/+lsp-lua.el
@@ -1,38 +1,38 @@
 ;;; modes/lsp-mode/+lsp-lua.el -*- lexical-binding: t; -*-
 
-(use-package lsp-mode
-  :ensure t
-  :commands lsp
-  :hook ((lua-mode) . lsp)
-  :config)
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :commands lsp
+;;   :hook ((lua-mode) . lsp)
+;;   :config)
 
-(use-package company-lsp
-  ;;:ensure t
-  :after lsp-mode
-  :config
-  (setq company-lsp-enable-recompletion t)
-  ;(push company-lsp company-backends)
-  (setq lsp-auto-configure nil))
+;; (use-package company-lsp
+;;   ;;:ensure t
+;;   :after lsp-mode
+;;   :config
+;;   (setq company-lsp-enable-recompletion t)
+;;   ;(push company-lsp company-backends)
+;;   (setq lsp-auto-configure nil))
 
-(defun set-company-backends-for-lua()
-  "Set lua company backend."
-  (setq-local company-backends
-              (quote
-               ((company-lsp
-                 company-lua
-                 company-keywords
-                 company-gtags
-                 company-yasnippet)
-                company-capf
-                company-dabbrev-code
-                company-files))))
+;; (defun set-company-backends-for-lua()
+;;   "Set lua company backend."
+;;   (setq-local company-backends
+;;               (quote
+;;                ((company-lsp
+;;                  company-lua
+;;                  company-keywords
+;;                  company-gtags
+;;                  company-yasnippet)
+;;                 company-capf
+;;                 company-dabbrev-code
+;;                 company-files))))
 
-(use-package lua-mode
-  ;;:ensure t
-  :mode "\\.lua$"
-  :interpreter "lua"
-  :hook (lua-mode . set-company-backends-for-lua)
-  :config
-  (setq lua-indent-level 4)
-  (setq lua-indent-string-contents t)
-  (setq lua-prefix-key nil))
+;; (use-package lua-mode
+;;   ;;:ensure t
+;;   :mode "\\.lua$"
+;;   :interpreter "lua"
+;;   :hook (lua-mode . set-company-backends-for-lua)
+;;   :config
+;;   (setq lua-indent-level 4)
+;;   (setq lua-indent-string-contents t)
+;;   (setq lua-prefix-key nil))
