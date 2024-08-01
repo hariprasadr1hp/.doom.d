1d688377403947227cf2ac116ca9a7ef7e5160c1
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2024-08-01 11:17:34 +0200

    add org-export options

--- modes/org_mode/+config.el
+++ modes/org_mode/+config.el
@@ -4,6 +4,7 @@
 (load! "+org_links")
 (load! "+org_journal")
 (load! "+org_roam")
+(load! "+org_export")
 (load! "+org_brain")
 (load! "+org_latex")
 (load! "+org_pomodoro")
---
+++ modes/org_mode/+org_export.el
@@ -0,0 +1,12 @@
+;;; modes/org-mode/+org_export.el -*- lexical-binding: t; -*-
+
+; (require 'org)
+;
+; (defun read-org-file-contents (file-path)
+;   "Read and display the contents of an Org file specified by FILE-PATH."
+;   (interactive "fPath to org file: ") ;; Prompts for the file path
+;   (with-temp-buffer
+;     (insert-file-contents file-path)
+;     ;; Optionally, you can perform some operations on the buffer here
+;     (message "%s" (buffer-string)))) ;; Print the contents in the minibuffer
+;
