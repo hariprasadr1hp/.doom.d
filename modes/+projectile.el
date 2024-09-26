79524fe6a6bdcf155f5630ca183ec1294d29ff16
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2024-09-26 09:52:11 +0200

    trim plugins
    keeping only necessary modules, that can be used in conjunction with
    `roam`

--- modes/+projectile.el
+++ modes/+projectile.el
@@ -1,4 +1,6 @@
 ;;; modes/+projectile.el -*- lexical-binding: t; -*-
 
 (after! projectile
-  (setq projectile-project-search-path '("~/my/projects/" "~/my/org")))
+  (setq projectile-project-search-path '("~/my/projects/" "~/my/org"))
+  (setq projectile-project-root-files-bottom-up (remove ".git"
+                                                        projectile-project-root-files-bottom-up)))
--- modes/elfeed_mode/+config.el
+++
@@ -1,4 +0,0 @@
-;;; modes/elfeed-mode/+config.el -*- lexical-binding: t; -*-
-
-(load! "+elfeed")
-(load! "+elfeed_links")
